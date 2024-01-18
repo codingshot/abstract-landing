d2a8f6fde8f4e5ae5ae1bc629dc8929add8d005b
codingshot <45281667+codingshot@users.noreply.github.com>
2024-01-18 01:14:07 -0500

    added mintbase slides

--- pages/[redirect].js
+++ pages/[redirect].js
@@ -32,6 +32,7 @@ const RedirectLinks = {
 "/pagoda": "https://banyan-collective.notion.site/Pagoda-s-Chain-Signatures-089368fa56e74f1a8494dbb8dc5643ff?pvs=4",
 "/submit": "https://everything.dev/efiz.near/widget/every.hashtag.view?hashtag=abstraction",
 "/100xdev-slides": "https://docs.google.com/presentation/d/1eOuaI1UEAWOYBGjQjAzCfMcaossQmDQOrt8KwvNcmw4/edit?usp=sharing",
+"/mintbase-slides": "https://docs.google.com/presentation/d/e/2PACX-1vQ2pWknAxDRSXIRgbHdaWX-MEYa_etQDeBPdI0JwLmCqpLj3sxA9alF-UrRwBdLE8GQiOs-g6bMftOR/pub?start=false&loop=false&delayms=3000&slide=id.g20c6d8cafe6_0_1",
 
 
       
