bb94ce7c2bde14a2147d83914b0af9165f68d637
codingshot <45281667+codingshot@users.noreply.github.com>
2024-01-29 04:33:29 -0500

    added gallery link

--- pages/[redirect].js
+++ pages/[redirect].js
@@ -31,6 +31,7 @@ const RedirectLinks = {
 "/aggregation": " https://docs.google.com/document/d/1c2SWUo3yesoiqSDa0rzUtaKEi74MO5fYjN5U3DLdsQo/edit?usp=sharing",
 "/pagoda": "https://banyan-collective.notion.site/Pagoda-s-Chain-Signatures-089368fa56e74f1a8494dbb8dc5643ff?pvs=4",
 "/submit": "https://near.social/#/buildbox.near/widget/AbstractionHacks.Submit",
+"/gallery": "https://near.social/buildbox.near/widget/app?page=browse",
 "/100xdev-slides": "https://docs.google.com/presentation/d/1eOuaI1UEAWOYBGjQjAzCfMcaossQmDQOrt8KwvNcmw4/edit?usp=sharing",
 "/mintbase-slides": "https://docs.google.com/presentation/d/e/2PACX-1vQ2pWknAxDRSXIRgbHdaWX-MEYa_etQDeBPdI0JwLmCqpLj3sxA9alF-UrRwBdLE8GQiOs-g6bMftOR/pub?start=false&loop=false&delayms=3000&slide=id.g20c6d8cafe6_0_1",
 
